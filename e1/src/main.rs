65a387f4a853a2a60cd27dda0f66c1f098c2f498
Alexandre Leblanc <a.leblanc@mrtryhard.info>
2022-12-02 18:03:17 -0500

    AOC2022: Use file-based input.

--- e1/src/main.rs
+++ e1/src/main.rs
@@ -1,7 +1,9 @@
-fn get_most_calories(input: &str) -> u32 {
-    let elves_items = input.split("\n\n");
+use std::fs;
 
-    let a = elves_items.map(|elve_items| elve_items.split("\n")
+fn get_most_calories(input: &str) -> u32 {
+    let elves_items = input.split("\r\n\r\n");
+
+    let a = elves_items.map(|elve_items| elve_items.split("\r\n")
             .map(|item| item.parse::<u32>().unwrap_or(0))
             .sum())
         .max()
@@ -11,17 +13,19 @@ fn get_most_calories(input: &str) -> u32 {
 }
 
 fn main() {
-    let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
-    println!("{}", get_most_calories(input))
+    let input = fs::read_to_string("input.txt").expect("Needs an input file.");
+
+    println!("{}", get_most_calories(input.as_str()))
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
     use crate::get_most_calories;
 
     #[test]
     fn tst_finds_elf_with_most_calories() {
-        let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+        let input = fs::read_to_string("tst_input.txt").expect("Needs an input file.");
 
         assert_eq!(get_most_calories(&input), 24000);
     }
