bd75812fc02b34d27eaedaaa6e0a70430fb1a9f2
Alexandre Leblanc <a.leblanc@mrtryhard.info>
2022-12-02 19:53:52 -0500

    AOC2022: Add part 2 (sum of top 3 calories elves)

--- e1/src/main.rs
+++ e1/src/main.rs
@@ -1,32 +1,55 @@
 use std::fs;
 
-fn get_most_calories(input: &str) -> u32 {
+fn get_calories_per_elves(input: &str) -> Vec<u32> {
     let elves_items = input.split("\r\n\r\n");
 
-    let a = elves_items.map(|elve_items| elve_items.split("\r\n")
-            .map(|item| item.parse::<u32>().unwrap_or(0))
-            .sum())
-        .max()
-        .unwrap_or(0);
+    elves_items
+        .map(|elf_items| {
+            elf_items
+                .split("\r\n")
+                .map(|item| item.parse::<u32>().unwrap_or(0))
+                .sum()
+        })
+        .collect::<Vec<u32>>()
+}
 
-    a
+fn get_top_three_most_calories(input: &str) -> u32 {
+    let mut calories_per_elf = get_calories_per_elves(input);
+    calories_per_elf.sort_by(|a, b| b.cmp(a));
+    calories_per_elf.truncate(3);
+    calories_per_elf.iter().sum()
+}
+
+fn get_most_calories(input: &str) -> u32 {
+    get_calories_per_elves(input).iter().max().unwrap().clone()
 }
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Needs an input file.");
 
-    println!("{}", get_most_calories(input.as_str()))
+    println!("Most calories: {}", get_most_calories(input.as_str()));
+    println!(
+        "Sum of top 3 most calories: {}",
+        get_top_three_most_calories(input.as_str())
+    );
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::{get_most_calories, get_top_three_most_calories};
     use std::fs;
-    use crate::get_most_calories;
 
     #[test]
     fn tst_finds_elf_with_most_calories() {
         let input = fs::read_to_string("tst_input.txt").expect("Needs an input file.");
 
         assert_eq!(get_most_calories(&input), 24000);
+    }
+
+    #[test]
+    fn tst_sum_of_top_3_elves_with_most_calories() {
+        let input = fs::read_to_string("tst_input.txt").expect("Needs an input file.");
+
+        assert_eq!(get_top_three_most_calories(&input), 45000);
     }
 }
